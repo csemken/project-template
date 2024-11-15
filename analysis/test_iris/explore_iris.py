5c9211e6c8da35be30f026ea9d8c443bb0f0dcbe
csemken <csemken@users.noreply.github.com>
2024-11-15 11:39:07 -0800

    Fix path in explore_iris.ipynb

--- analysis/test_iris/explore_iris.py
+++ analysis/test_iris/explore_iris.py
@@ -6,9 +6,9 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.14.6
+#       jupytext_version: 1.16.4
 #   kernelspec:
-#     display_name: .venv
+#     display_name: Python 3
 #     language: python
 #     name: python3
 # ---
@@ -16,7 +16,7 @@
 import pandas as pd
 
 df_iris = pd.read_csv(
-    "../../../data/raw/test_iris/iris.csv",
+    "data/raw/test_iris/iris.csv",
     names=["sepal_length", "sepal_width", "pedal_length", "pedal_width", "class"],
 )
 df_iris.head(2)
